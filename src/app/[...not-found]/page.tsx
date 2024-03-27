62c7cf61cf1c3e902093cadda6c548483ec1339f
Michael Chuang <zhaungsont@gmail.com>
2024-03-28 02:03:45 +0800

    feat: 404

---
+++ src/app/[...not-found]/page.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import styles from './page.module.scss';
+import Image from 'next/image';
+export default function Custom404() {
+	return (
+		<div className={styles.custom404}>
+			<h1>404 - Page Not Found</h1>
+			<div className={styles.image}>
+				<Image
+					src="/images/shock-black-dude.JPG"
+					alt="Shocked black dude meme"
+					width={300}
+					height={300}
+				/>
+			</div>
+			<Link href="/">(Go home)</Link>
+		</div>
+	);
+}
