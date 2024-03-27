2e2b48af86063e8403ad305ab9a6a897de9fa92f
Michael Chuang <zhaungsont@gmail.com>
2024-03-28 03:42:13 +0800

    style: update 404

--- src/app/[...not-found]/page.tsx
+++ src/app/[...not-found]/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.scss';
 import Image from 'next/image';
 export default function Custom404() {
 	return (
-		<div className={styles.custom404}>
+		<div className={`${styles.custom404} page-404`}>
 			<h1>404 - Page Not Found</h1>
 			<div className={styles.image}>
 				<Image
--- src/components/ThemeToggleButton/ThemeToggleButton.client.tsx
+++ src/components/ThemeToggleButton/ThemeToggleButton.client.tsx
@@ -2,16 +2,24 @@
 import React, { useState } from 'react';
 import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
 import styles from './ThemeToggleButton.module.scss';
+import { useRouter } from 'next/navigation';
 
 export default function ThemeToggleButton() {
 	const [isDarkTheme, setIsDarkTheme] = useState(false);
+	const [clickedTimes, setClickedTimes] = useState(0);
+	const router = useRouter();
+
 	const toggleTheme = () => {
+		setClickedTimes((prevTimes) => prevTimes + 1);
 		setIsDarkTheme(!isDarkTheme);
 
 		document.body.classList.toggle('light-theme');
 		document.documentElement.classList.toggle('light-theme');
 	};
 
+	if (clickedTimes === 10) {
+		router.push('/dashboard', { scroll: false });
+	}
 	return (
 		<button
 			className={styles.themeButton}
