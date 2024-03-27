cd78c19a4a81033b6683b5b1a24bf388e864f1a3
Michael Chuang <zhaungsont@gmail.com>
2024-03-27 23:08:13 +0800

    feat: theme toggle function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,25 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Fira_Mono } from 'next/font/google';
 import './globals.scss';
 import Link from 'next/link';
 import styles from './page.module.scss';
 import { FiExternalLink } from 'react-icons/fi';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import ThemeToggleButton from '@/components/ThemeToggleButton.client';
 
 const inter = Inter({ subsets: ['latin'] });
-
+const firaMono = Fira_Mono({
+	subsets: ['latin'],
+	weight: ['400', '500', '700'],
+});
 export const metadata: Metadata = {
 	title: 'Michael Chuang',
 	description: "Michael Chuang's personal website",
+	keywords: ['personal website', 'portfolio', 'software engineer'],
+};
+
+const toggleTheme = () => {
+	document.body.classList.toggle('dark-theme');
 };
 
 export default function RootLayout({
@@ -20,14 +30,25 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${inter.className} body`}>
+				<div className={styles.themeToggle}>
+					<ThemeToggleButton />
+				</div>
 				{children}
 				<main className={`${styles.main} homepage`}>
 					<div className={styles.left}>
 						<h1>Michael Chuang</h1>
 						<h3>Software Engineer</h3>
 						<h3>Taipei, Taiwan</h3>
+						<div className={styles.links}>
+							<a target="_blank" href="https://github.com/zhaungsont">
+								<FaGithub />
+							</a>
+							<a target="_blank" href="https://www.linkedin.com/in/zhsont/">
+								<FaLinkedin />
+							</a>
+						</div>
 					</div>
-					<div className={styles.right}>
+					<div className={`${firaMono.className} ${styles.right}`}>
 						<Link href="/about">About</Link>
 						<Link href="/experience">Experience</Link>
 						<Link href="/resume">Resume</Link>
---
+++ src/components/ThemeToggleButton.client.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React, { useState } from 'react';
+import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import styles from './ThemeToggleButton.module.scss';
+
+export default function ThemeToggleButton() {
+	const [isDarkTheme, setIsDarkTheme] = useState(false);
+	const toggleTheme = () => {
+		setIsDarkTheme(!isDarkTheme);
+		document.body.classList.toggle('dark-theme');
+	};
+
+	return (
+		<button
+			className={styles.themeButton}
+			onClick={toggleTheme}
+			aria-label={isDarkTheme ? 'Switch to light mode' : 'Switch to dark mode'}
+		>
+			{isDarkTheme ? <MdOutlineDarkMode /> : <MdOutlineLightMode />}
+		</button>
+	);
+}
