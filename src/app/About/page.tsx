0c4508af27549fa523025740fa730961e75dd405
Michael Chuang <zhaungsont@gmail.com>
2024-03-28 02:26:54 +0800

    chore: update bio

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -19,8 +19,15 @@ export default function About() {
 					table.
 				</p>
 				<p className={styles.bio}>
-					Michael&apos;s expertise lies in the front-end ecosystem, where he
-					specializes in TypeScript, React, Redux, and Node.js. He is also
+					Michael&apos;s journey into the world of technology began at the age
+					of 13 when he first explored coding through modding for the video game
+					Grand Theft Auto and building static websites. Despite majoring in
+					English literature during university, his fascination with coding led
+					him to pursue a career in software engineering.
+				</p>
+				<p className={styles.bio}>
+					Now, Michael&apos;s expertise lies in the front-end ecosystem, where
+					he specializes in TypeScript, React, Redux, and Node.js. He is also
 					proficient in Python, unit testing, and continuous integration and
 					deployment (CI/CD) practices.
 				</p>
