28d310a86e39075bdc696e964685169d4ea51aca
Michael Chuang <zhaungsont@gmail.com>
2024-03-27 22:13:39 +0800

    feat: pure CSS page blur

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 import styles from './page.module.scss';
 import Link from 'next/link';
 
-export default function Card() {
+export default function About() {
 	return (
-		<div className={styles.test}>
+		<div className={`${styles.about} subpage`}>
 			<h1>About</h1>
 			<Link href="/">Home</Link>
 		</div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.scss';
 import Link from 'next/link';
 import styles from './page.module.scss';
+import { FiExternalLink } from 'react-icons/fi';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +19,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className}`}>
+			<body className={`${inter.className} body`}>
 				{children}
-				<main className={styles.main}>
+				<main className={`${styles.main} homepage`}>
 					<div className={styles.left}>
 						<h1>Michael Chuang</h1>
 						<h3>Software Engineer</h3>
@@ -30,7 +31,10 @@ export default function RootLayout({
 						<Link href="/about">About</Link>
 						<Link href="/experience">Experience</Link>
 						<Link href="/resume">Resume</Link>
-						<Link href="https://blog.zhsont.cc/">Blog</Link>
+						<a target="_blank" href="https://blog.zhsont.cc/">
+							Blog
+							<FiExternalLink />
+						</a>
 					</div>
 				</main>
 			</body>
