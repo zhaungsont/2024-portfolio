2698ba16cb61abbc551c8ab7eaa1e5d173689c0f
Michael Chuang <zhaungsont@gmail.com>
2024-04-01 15:31:44 +0800

    refactor: eslint, stylelint

--- src/app/components/ExternalLink/ExternalLink.tsx
+++ src/app/components/ExternalLink/ExternalLink.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { FiExternalLink } from 'react-icons/fi';
+
 import styles from './ExternalLink.module.scss';
 
 export default function ExternalLink({
-	href,
-	children,
+  href,
+  children,
 }: {
-	href: string;
-	children: React.ReactNode;
+  href: string;
+  children: React.ReactNode;
 }) {
-	return (
-		<a target="_blank" href={href} className={styles.externalLink}>
-			{children}
-			<FiExternalLink />
-		</a>
-	);
+  return (
+    <a
+      target="_blank"
+      href={href}
+      className={styles.externalLink}
+      rel="noreferrer"
+    >
+      {children}
+      <FiExternalLink />
+    </a>
+  );
 }
--- src/app/yeet/page.tsx
+++ src/app/yeet/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import SubPage from '@/app/ui/SubPage/SubPage';
+
 import Image from 'next/image';
 
+import SubPage from '@/app/ui/SubPage/SubPage';
+
 export default function Yeet() {
-	return (
-		<SubPage>
-			<Image src="/images/elmo-flame.gif" alt="Elmo on fire" fill={true} />
-		</SubPage>
-	);
+  return (
+    <SubPage>
+      <Image src="/images/elmo-flame.gif" alt="Elmo on fire" fill />
+    </SubPage>
+  );
 }
